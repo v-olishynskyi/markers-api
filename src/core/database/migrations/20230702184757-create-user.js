7690f27ace9eefb060eaad9f647f4d8777d61b40
Vladyslav Olishynskyi <vlad.olishynskyi.work@gmail.com>
2023-07-04 23:04:08 +0300

    add sequelize config for migrations

---
+++ src/core/database/migrations/20230702184757-create-user.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        unique: { name: 'id', msg: 'must be unique' },
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: { name: 'email', msg: 'must be unique' },
+      },
+      password: { type: Sequelize.STRING, allowNull: false, unique: false },
+      first_name: { type: Sequelize.STRING, allowNull: false, unique: false },
+      last_name: { type: Sequelize.STRING, allowNull: false, unique: false },
+      username: { type: Sequelize.STRING, allowNull: false, unique: true },
+      middle_name: { type: Sequelize.STRING, allowNull: false, unique: false },
+      avatar_url: { type: Sequelize.STRING, allowNull: false, unique: true },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Users');
+  },
+};
